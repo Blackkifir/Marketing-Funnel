d8d6917f7310783974be03a5aa40fc61196ba79d
Nikolay <goldlamborgini@gmail.com>
2024-01-24 15:19:29 +0200

    designed section achievements

--- pages/marketingCenter/index.tsx
+++ pages/marketingCenter/index.tsx
@@ -109,6 +109,31 @@ export default function MarketingCenter() {
             </div>
           </div>
         </section>
+        <section className={styles.achievementsContainer}>
+          <h2 className={styles.achievementsFlexBlock__title}>
+            People just like you achieved great results with App!
+          </h2>
+          <div className={styles.achievementsFlexBlock}>
+            <div className={styles.achievementsFlexBlock__item}>
+              <span className={styles.achievementsFlexBlock__percent}>71%</span>
+              <p className={styles.achievementsFlexBlock__text}>
+                of users were able to feel the power of prayer after just 6 weeks
+              </p>
+            </div>
+            <div className={styles.achievementsFlexBlock__item}>
+              <span className={styles.achievementsFlexBlock__percent}>65%</span>
+              <p className={styles.achievementsFlexBlock__text}>
+                of users started to have a meaningful daily routine and peaceful mind
+              </p>
+            </div>
+            <div className={styles.achievementsFlexBlock__item}>
+              <span className={styles.achievementsFlexBlock__percent}>47%</span>
+              <p className={styles.achievementsFlexBlock__text}>
+                of users struggled with finding time for daily praying
+              </p>
+            </div>
+          </div>
+        </section>
         <Footer />
       </div>
     </div>
